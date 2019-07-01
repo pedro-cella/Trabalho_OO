b0f686c3304a4c787c1ccdedac90349c15872b42
Pedro Cella <49076513+pedro-cella@users.noreply.github.com>
2019-06-30 23:44:16 -0300

    Criacao das classes de Combustivel e Despesas

---
+++ manutencao/Combustivel.java
@@ -0,0 +1,65 @@
+package manutencao;
+
+public class Combustivel {
+protected String data_abastecimento;
+protected String tipo_combustivel;
+protected int kilometragem;
+public float valor_combustivel;
+public float valor_total;
+protected String tipo_abastecimento;
+
+public Combustivel(){
+	
+}
+
+public String getData_abastecimento() {
+	return data_abastecimento;
+}
+
+public void setData_abastecimento(String data_abastecimento) {
+	this.data_abastecimento = data_abastecimento;
+}
+
+public String getTipo_combustivel() {
+	return tipo_combustivel;
+}
+
+public void setTipo_combustivel(String tipo_combustivel) {
+	this.tipo_combustivel = tipo_combustivel;
+}
+
+public int getKilometragem() {
+	return kilometragem;
+}
+
+public void setKilometragem(int kilometragem) {
+	this.kilometragem = kilometragem;
+}
+
+public float getValor_combustivel() {
+	return valor_combustivel;
+}
+
+public void setValor_combustivel(float valor_combustivel) {
+	this.valor_combustivel = valor_combustivel;
+}
+
+public float getValor_total() {
+	return valor_total;
+}
+
+public void setValor_total(float valor_total) {
+	this.valor_total = valor_total;
+}
+
+public String getTipo_abastecimento() {
+	return tipo_abastecimento;
+}
+
+public void setTipo_abastecimento(String tipo_abastecimento) {
+	this.tipo_abastecimento = tipo_abastecimento;
+}
+
+
+
+}
---
+++ manutencao/Despesas.java
@@ -0,0 +1,40 @@
+package manutencao;
+public class Despesas extends Combustivel{ 
+public String descricao;
+public int ano_despesa;
+public String categoria;
+
+Despesas(){
+	
+}
+
+public String getDescricao() {
+	return descricao;
+}
+
+public void setDescricao(String descricao) {
+	this.descricao = descricao;
+}
+
+public int getAno_despesa() {
+	return ano_despesa;
+}
+
+public void setAno_despesa(int ano_despesa) {
+	this.ano_despesa = ano_despesa;
+}
+
+public String getCategoria() {
+	return categoria;
+}
+
+public void setCategoria(String categoria) {
+	this.categoria = categoria;
+}
+
+
+
+
+}
+
+
