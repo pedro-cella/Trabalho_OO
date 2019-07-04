94245e53fc8c66b02aeaa719cd7c41a4affd4b2a
Pedro Cella <49076513+pedro-cella@users.noreply.github.com>
2019-07-04 15:28:29 -0300

    Mudancas de impressao

--- manutencao/Despesas.java
+++ manutencao/Despesas.java
@@ -1,7 +1,9 @@
 package manutencao;
 //Classe Abstrata, ou seja, nao posuui um metodo construtor;
 
-public class Despesas{ 
+import excessoes.Excessoes;
+
+public class Despesas extends Excessoes{ 
 public String nome_despesa;
 public String ano_despesa;
 public String categoria;
