7a1261da0b310720c2f439cb96290a1a3430c9a2
Pedro Cella <49076513+pedro-cella@users.noreply.github.com>
2019-07-08 18:53:03 -0300

    Atualizacao

--- manutencao/Combustivel.java
+++ manutencao/Combustivel.java
@@ -1,4 +1,8 @@
 package manutencao;
+import excessoes.DescricaoEmBrancoException;
+import excessoes.ValorInvalidoException;
+import veiculo.Veiculo;
+import excessoes.CombustivelIncompativelException;
 public class Combustivel extends Despesas{
 protected String data_abastecimento;
 protected String tipo_combustivel;
@@ -6,6 +10,7 @@ protected int kilometragem;
 public float valor_combustivel;
 public float valor_total;
 protected String tipo_abastecimento;
+public Veiculo v;
 
 public Combustivel(){
 	
@@ -15,16 +20,26 @@ public String getData_abastecimento() {
 	return data_abastecimento;
 }
 
-public void setData_abastecimento(String data_abastecimento) {
-	this.data_abastecimento = data_abastecimento;
+public void setData_abastecimento(String data_abastecimento) throws DescricaoEmBrancoException {
+	if(data_abastecimento.isEmpty()) {
+		throw new DescricaoEmBrancoException("Descrição em branco. Por favor insira um dado inválido!");
+	}else {
+		this.data_abastecimento = data_abastecimento;
+	}
 }
 
 public String getTipo_combustivel() {
 	return tipo_combustivel;
 }
 
-public void setTipo_combustivel(String tipo_combustivel) {
-	this.tipo_combustivel = tipo_combustivel;
+public void setTipo_combustivel(String tipo_combustivel) throws DescricaoEmBrancoException, CombustivelIncompativelException{
+	if(tipo_combustivel.isEmpty()) {
+		throw new DescricaoEmBrancoException("Descrição em branco. Por favor insira um dado inválido!");
+	}else if(this.tipo_combustivel != v.getCombustivel()) {
+		throw new CombustivelIncompativelException("Esse veiculo nao aceita esse tipo de combustivel!");
+	}else {
+		this.tipo_combustivel = tipo_combustivel;
+	}
 }
 
 public int getKilometragem() {
