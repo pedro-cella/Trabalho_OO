931e220bafb7d6dc9d634d72e1b3a55bcaca9417
Pedro Cella <49076513+pedro-cella@users.noreply.github.com>
2019-07-08 20:04:18 -0300

    Atualizacao com excessoes

--- manutencao/Combustivel.java
+++ manutencao/Combustivel.java
@@ -10,7 +10,6 @@ protected int kilometragem;
 public float valor_combustivel;
 public float valor_total;
 protected String tipo_abastecimento;
-public Veiculo v;
 
 public Combustivel(){
 	
@@ -35,7 +34,7 @@ public String getTipo_combustivel() {
 public void setTipo_combustivel(String tipo_combustivel) throws DescricaoEmBrancoException, CombustivelIncompativelException{
 	if(tipo_combustivel.isEmpty()) {
 		throw new DescricaoEmBrancoException("Descrição em branco. Por favor insira um dado inválido!");
-	}else if(this.tipo_combustivel != v.getCombustivel()) {
+	}else if(this.tipo_combustivel != tipo_combustivel) {
 		throw new CombustivelIncompativelException("Esse veiculo nao aceita esse tipo de combustivel!");
 	}else {
 		this.tipo_combustivel = tipo_combustivel;
@@ -46,32 +45,48 @@ public int getKilometragem() {
 	return kilometragem;
 }
 
-public void setKilometragem(int kilometragem) {
-	this.kilometragem = kilometragem;
+public void setKilometragem(int kilometragem) throws ValorInvalidoException{
+	if(this.kilometragem > kilometragem) {
+		throw new ValorInvalidoException("Valor de kilometragem inválido!");
+	}else {
+		this.kilometragem = kilometragem;
+	}
 }
 
 public float getValor_combustivel() {
 	return valor_combustivel;
 }
 
-public void setValor_combustivel(float valor_combustivel) {
-	this.valor_combustivel = valor_combustivel;
+public void setValor_combustivel(float valor_combustivel) throws ValorInvalidoException{
+	if(valor_combustivel <= 0) {
+		throw new ValorInvalidoException("Valor de combustivel inválido!");
+	}else {
+		this.valor_combustivel = valor_combustivel;
+	}
 }
 
 public float getValor_total() {
 	return valor_total;
 }
 
-public void setValor_total(float valor_total) {
-	this.valor_total = valor_total;
+public void setValor_total(float valor_total) throws ValorInvalidoException{
+	if(valor_total <= 0) {
+		throw new ValorInvalidoException("Valor de abastecimento inválido!");
+	}else {
+		this.valor_total = valor_total;
+	}
 }
 
-public String getTipo_abastecimento() {
+public String getTipo_abastecimento(){
 	return tipo_abastecimento;
 }
 
-public void setTipo_abastecimento(String tipo_abastecimento) {
-	this.tipo_abastecimento = tipo_abastecimento;
+public void setTipo_abastecimento(String tipo_abastecimento) throws DescricaoEmBrancoException{
+	if(tipo_abastecimento.isEmpty()) {
+		throw new DescricaoEmBrancoException("Descrição em branco. Por favor insira um dado inválido!");
+	}else {
+		this.tipo_abastecimento = tipo_abastecimento;
+	}
 }
 
 }
