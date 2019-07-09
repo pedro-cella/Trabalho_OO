70b19ac062a197c34609b8efbce859d275586afc
Pedro Cella <49076513+pedro-cella@users.noreply.github.com>
2019-07-09 11:53:58 -0300

    Criacao das excessoes e atualizacao do pacote

--- excessoes/DescricaoEmBrancoException.java
+++ excessoes/DescricaoEmBrancoException.java
@@ -1,13 +1,8 @@
 package excessoes;
-import javax.swing.JOptionPane;
 
 public class DescricaoEmBrancoException extends Exception{
 	
-	public String DescricaoEmBrancoException(String descricao){
-		while(descricao == null) {
-			descricao = JOptionPane.showInputDialog("Por favor, digite uma descricao valida: \n");
-		
-		}
-			return descricao;
+	public DescricaoEmBrancoException(String message){
+		super(message);
 	}
 }
---
+++ excessoes/ValorInvalidoException.java
@@ -0,0 +1,8 @@
+package excessoes;
+
+public class ValorInvalidoException extends Exception{
+	
+	public ValorInvalidoException(String message){
+        super(message);
+	}
+}
